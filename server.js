58d76b5fa80c69bf4c89a3a6e7e8894c2187cb82
shivam <shivam.kaushik@outlook.com>
2020-04-06 14:56:51 +0530

    Finish step10 of the phonebook backend challenge

--- server.js
+++ server.js
@@ -6,34 +6,35 @@ const morgan = require('morgan');
 app.use(express.json());
 
 // morgan
-morgan.token('body', function(req, res) {
+morgan.token('body', function (req, res) {
   return JSON.stringify(req.body);
 });
 
 app.use(morgan(':method :url :status :response-time ms - :body'));
+
 app.use(express.static('build'));
 
 let persons = [
   {
     name: 'Arto Hellas',
     number: '982939393',
-    id: 1
+    id: 1,
   },
   {
     name: 'Ada Lovelace',
     number: '5353453453',
-    id: 2
+    id: 2,
   },
   {
     name: 'Dan Abramov',
     number: '3534534534',
-    id: 3
+    id: 3,
   },
   {
     name: 'Mary Poppendieck',
     number: '4535345343',
-    id: 4
-  }
+    id: 4,
+  },
 ];
 
 app.get('/api/persons', (req, res) => {
@@ -52,17 +53,17 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id * 1;
-  const person = persons.find(el => el.id === id);
+  const person = persons.find((el) => el.id === id);
   if (person) {
     res.status(200).json(person);
   } else {
-    res.status(400).end();
+    res.status(404).end();
   }
 });
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id * 1;
-  persons = persons.filter(person => person.id !== id);
+  persons = persons.filter((person) => person.id !== id);
   res.status(204).end();
 });
 
@@ -71,22 +72,22 @@ app.post('/api/persons', (req, res) => {
 
   if (!name || !number) {
     return res.status(400).json({
-      error: 'name/number missing'
+      error: 'name/number missing',
     });
   }
 
-  const existingPerson = persons.find(person => person.name === name);
+  const existingPerson = persons.find((person) => person.name === name);
 
   if (existingPerson) {
     return res.status(400).json({
-      error: 'name must be unique'
+      error: 'name must be unique',
     });
   }
 
   const newPerson = {
     name,
     number,
-    id: uuid() // used uuid instead of Math.random for more robustness
+    id: uuid(), // used uuid instead of Math.random for more robustness
   };
   persons = [...persons, newPerson];
 
