7a514caf0ee7a18a923c6d58023b0167fba5e02f
shivam <shivam.kaushik@outlook.com>
2020-04-06 18:07:26 +0530

    Finish step8 phonebook challenge

--- models/person.js
+++ models/person.js
@@ -1,18 +1,29 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
+    useCreateIndex: true,
   })
   .then(() => console.log('DB connected'))
   .catch((err) => console.log('DB connection failed', err));
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+    unique: true,
+  },
+  number: {
+    type: String,
+    required: [true, 'A user must have a number'],
+  },
 });
+
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
